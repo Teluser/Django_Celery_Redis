fa16dc6936cdf2a654a7a9e55ef1e047c24c28c5
Teluser <57626765+Teluser@users.noreply.github.com>
2024-01-09 11:28:03 +0700

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c#+_(#stq2pt+%40&8^tm$tu@=8l38#(fa=oyn+3yt=hk@!!p*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
